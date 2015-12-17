bae3805092d9b9530c366fd06ac7163647b1919c
Jacek Jabłonka <jacjab@gmail.com>
2015-12-17 15:53:56 +0100

    Security? :)

---
+++ src/com/company/security/SecurityFunctionTwo.java
@@ -0,0 +1,12 @@
+package com.company.security;
+
+/**
+ * Created by Jacek on 2015-12-17.
+ */
+public class SecurityFunctionTwo {
+    public static void main(String[] args) {
+        // power of number
+        int x = 1;
+        System.out.println(x*x);
+    }
+}
---
+++ src/com/company/security/SecurityFunctionsOne.java
@@ -0,0 +1,13 @@
+package com.company.security;
+
+/**
+ * Created by Jacek on 2015-12-16.
+ */
+public class SecurityFunctionsOne {
+    public static void main(String[] args) {
+        // division reminder
+        System.out.println(13/11);
+        System.out.println(13f/11f);
+        System.out.println(13%11);
+    }
+}
